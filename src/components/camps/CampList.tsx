503738f8f6ce79ba7a9b2c95e446900ec1d44992
jinnycrab <jinyi95@hotmail.sg>
2025-02-16 15:20:03 +0800

    Update CampList.tsx

--- src/components/camps/CampList.tsx
+++ src/components/camps/CampList.tsx
@@ -19,7 +19,7 @@ const CampList = ({ camps }: CampListProps) => {
   return (
     <section>
       <div className="mb-8">
-        <h2 className="text-3xl font-bold mb-8 font-general-sans">Explore Our 2025 Camps</h2>
+        <h2 className="text-3xl font-bold mb-8 font-general-sans">Our 2025 Camps</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {camps.map((camp) => (
             <Link
