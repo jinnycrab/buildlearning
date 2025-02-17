c355bdc8362dbe06ae650744ac653b470619a67a
jinnycrab <jinyi95@hotmail.sg>
2025-02-17 23:50:46 +0800

    Update Pedagogy.tsx

--- src/pages/Pedagogy.tsx
+++ src/pages/Pedagogy.tsx
@@ -60,7 +60,7 @@ const Process = () => {
         "Explore the multi-faceted nature of the problem",
         "Define project scope, objectives and expectations"
       ],
-      image: "/placeholder.svg"
+      image: "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
       letter: "U",
