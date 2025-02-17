6432a1be045bdb8d05915b1c864cd600e75ec528
jinnycrab <jinyi95@hotmail.sg>
2025-02-17 18:45:37 +0800

    Update Process.tsx
    trying out putting an image

--- src/pages/Process.tsx
+++ src/pages/Process.tsx
@@ -163,7 +163,7 @@ const Process = () => {
             </div>
             <div>
               <img 
-                src="/placeholder.svg"
+                src="https://images.unsplash.com/photo-1526378787940-576a539ba69d?q=80&w=2969&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Innovation Process"
                 className="rounded-2xl shadow-lg w-full"
               />
