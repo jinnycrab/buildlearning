2dc571d83bb24ff61d33e431007941b67362062a
jinnycrab <jinyi95@hotmail.sg>
2025-02-18 13:38:45 +0800

    Update Index.tsx
    Changing why build to what is build for clarity. So that I can later create a why build section for the problems I'm solving.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -45,7 +45,7 @@ const Index = () => {
       <section className="py-24 bg-primary-foreground">
         <div className="container mx-auto px-4">
           <h2 className="text-4xl md:text-5xl font-bold mb-12 text-center font-general-sans">
-            Why Build?
+            What is Build?
           </h2>
           <Features />
         </div>
