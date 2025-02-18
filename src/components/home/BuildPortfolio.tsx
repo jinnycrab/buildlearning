977c496847ba04b55ea79cb575e2d1d0d1721d63
jinnycrab <jinyi95@hotmail.sg>
2025-02-18 16:23:38 +0800

    Update BuildPortfolio.tsx
    Updating poster picture

--- src/components/home/BuildPortfolio.tsx
+++ src/components/home/BuildPortfolio.tsx
@@ -30,7 +30,7 @@ const BuildPortfolio = () => {
         >
           <div className="relative aspect-square w-full mb-8 overflow-hidden rounded-xl">
             <img
-              src="/placeholder.svg"
+              src="https://images.unsplash.com/photo-1562466002-f32b44d97a2e?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="Build a Poster Example"
               className="w-full h-full object-cover"
             />
