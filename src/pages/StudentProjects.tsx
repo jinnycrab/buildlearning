2e2e2bf4181066dc7f2c42ef8f61a486e7c4754d
jinnycrab <jinyi95@hotmail.sg>
2025-02-20 19:19:24 +0800

    Update StudentProjects.tsx
    uploading poster

--- src/pages/StudentProjects.tsx
+++ src/pages/StudentProjects.tsx
@@ -13,7 +13,7 @@ const StudentProjects = () => {
     {
       id: 1,
       title: "Project Tai: Designing AI for Education",
-      posterImage: "https://raw.githubusercontent.com/jinnycrab/holiday-learn-fun-09/refs/heads/main/images/How%20to%20make%20a%20research%20poster%20A%20guide%20for%20students.jpg?token=GHSAT0AAAAAAC6ZUQEPEDUE24HHMDTKMM5QZ5XBS5A", 
+      posterImage: "https://raw.githubusercontent.com/jinnycrab/holiday-learn-fun-09/refs/heads/main/images/How%20to%20make%20a%20research%20poster%20A%20guide%20for%20students.jpg?token=GHSAT0AAAAAAC7I2BW74XPTLEORUZ2GKEXWZ5XB7IA", 
       problemStatement: "Teachers are burnt out from marking scripts, and this can create gaps in students learning. How can we leverage AI to reduce teacher fatigue from marking?",
       proposedSolution: "AI assistant with in-built student dashboard and personalized feedback generator",
       demoLink: "Link provided upon request to protect student privacy",
@@ -21,7 +21,7 @@ const StudentProjects = () => {
     {
       id: 2,
       title: "Project Tai: Designing AI for Education",
-      posterImage: "https://raw.githubusercontent.com/jinnycrab/holiday-learn-fun-09/refs/heads/main/images/How%20to%20make%20a%20research%20poster%20A%20guide%20for%20students.jpg?token=GHSAT0AAAAAAC6ZUQEPEDUE24HHMDTKMM5QZ5XBS5A",
+      posterImage: "https://raw.githubusercontent.com/jinnycrab/holiday-learn-fun-09/refs/heads/main/images/How%20to%20make%20a%20research%20poster%20A%20guide%20for%20students.jpg?token=GHSAT0AAAAAAC7I2BW74XPTLEORUZ2GKEXWZ5XB7IA",
       problemStatement: "Teachers are burnt out from marking scripts, and this can create gaps in students learning. How can we leverage AI to reduce teacher fatigue from marking?",
       proposedSolution: "AI assistant with in-built student dashboard and personalized feedback generator",
       demoLink: "Link provided upon request to protect student privacy",
