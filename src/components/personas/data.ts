c9ed368a508da78bb834e9e1195ad44de5d069e4
jinnycrab <jinyi95@hotmail.sg>
2025-02-26 11:28:50 +0800

    Update data.ts

--- src/components/personas/data.ts
+++ src/components/personas/data.ts
@@ -8,7 +8,7 @@ export const userPersonas: UserPersona[] = [
     problems: [
       "Demonstrate your passion and dedication for your choice of field with a project",
       "Use projects as talking points during interviews, sharing your thought process and growth",
-      "Differentiate yourself with tangible work you can upload during applications"
+      "Stand out with tangible work you can upload during applications"
     ],
     image: "https://images.unsplash.com/photo-1633061273472-7c62356c7329?q=80&w=3051&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
@@ -17,8 +17,8 @@ export const userPersonas: UserPersona[] = [
     description: "Turn your ideas into working prototypes, launching your entrepreneurship journey",
     problems: [
       "Overcome your inertia and knowledge gaps with guided prototyping",
-      "Learn how design thinking and research can help you create desirable products and services",
-      "Join a community of innovators and builders harnessing tools like AI to keep operations lean"
+      "Learn how design thinking and research can help you with your ventures",
+      "Join a community of innovators and builders harnessing AI tools"
     ],
     image: "https://images.unsplash.com/photo-1507591064344-4c6ce005b128?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
